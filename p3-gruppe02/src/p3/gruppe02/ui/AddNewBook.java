4acc8aa7a595da3450489662c6a305a423b0d8c9
JanRipken <janripken@gmx.net>
2022-11-03 14:05:21 +0100

    Klasse AddNewBook

--- p3-gruppe02/src/p3/gruppe02/ui/AddNewBook.java
+++ p3-gruppe02/src/p3/gruppe02/ui/AddNewBook.java
@@ -1,7 +1,9 @@
 
 package p3.gruppe02.ui;
 
+import java.awt.Font;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class AddNewBook {
     
@@ -9,9 +11,12 @@ public class AddNewBook {
     JLabel label = new JLabel("HI");
     AddNewBook(){
         
+        label.setBounds(0,0,100,50);
+        label.setFont(new Font(null,Font.PLAIN,25));
         
+        frame.add(label);
         
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
         frame.setSize(420, 420);
         frame.setLayout(null);
         frame.setVisible(true);
