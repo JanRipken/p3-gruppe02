bd3d43a442e1a10bdcbdb44d7acf37ecb7c6aaaa
JanRipken <janripken@gmx.net>
2022-11-03 14:08:42 +0100

    small refactoring

--- p3-gruppe02/src/p3/gruppe02/ui/AddNewBook.java
+++ p3-gruppe02/src/p3/gruppe02/ui/AddNewBook.java
@@ -7,18 +7,18 @@ import javax.swing.JLabel;
 
 public class AddNewBook {
     
-    JFrame frame = new JFrame();
-    JLabel label = new JLabel("HI");
+    JFrame addNewBookFrame = new JFrame();
+    JLabel testLabel = new JLabel("HI");
     AddNewBook(){
         
-        label.setBounds(0,0,100,50);
-        label.setFont(new Font(null,Font.PLAIN,25));
+        testLabel.setBounds(0,0,100,50);
+        testLabel.setFont(new Font(null,Font.PLAIN,25));
         
-        frame.add(label);
+        addNewBookFrame.add(testLabel);
         
        
-        frame.setSize(420, 420);
-        frame.setLayout(null);
-        frame.setVisible(true);
+        addNewBookFrame.setSize(420, 420);
+        addNewBookFrame.setLayout(null);
+        addNewBookFrame.setVisible(true);
     }
 }
