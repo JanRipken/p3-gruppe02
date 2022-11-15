6d50d8a03dee15b073fbbfb54846a46730ddcffd
JanRipken <72017165+JanRipken@users.noreply.github.com>
2022-11-15 13:57:12 +0100

    south panel aus der main klassse gezogen

--- p3-gruppe02/src/view/MainFrames/MainFrame.java
+++ p3-gruppe02/src/view/MainFrames/MainFrame.java
@@ -89,33 +89,7 @@ public class MainFrame extends JFrame {
 
     
 
-    private class SouthPanel extends JPanel {
-
-        public SouthPanel() {
-
-            this.setLayout(new GridLayout());
-
-            //test - 2 bücher erstellt um diese auslesen zu lassen
-            BookModel buch1 = new BookModel("BuchEins");
-            BookModel buch2 = new BookModel("BuchDos");
-            
-            //Erstellen der Tabelle
-            //Spalteninhalt
-            //test - Titel werden ausgelesen
-            String[][] data = {
-                {buch1.getTitel(),"Mustermann", "Max", "123", "true", "1.2", "1990"},
-                {buch2.getTitel(), "Musterfrau", "Maxine", "456", "false", "6.7", "2021"}
-            };
-            // Spaltennamen
-            String[] columnNames = {"Titel", "AutorName", "AutorVorname", "Seitenanzahl", "Gelesen", "Bewertung", "Erscheinungsjahr"};
-            // Initiallisierung
-            JTable tabelle = new JTable(data, columnNames);
-            // in eine ScrollPane einsetzen, macht die tabelle scrollbar
-            JScrollPane sp = new JScrollPane(tabelle);
-            this.add(sp);
-        }
-
-    }
+    
 
     public MainFrame() {
 
---
+++ p3-gruppe02/src/view/MainFrames/SouthPanel.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view.MainFrames;
+
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import model.BookModel;
+
+/**
+ *
+ * @author janri
+ */
+public class SouthPanel extends JPanel{
+    public SouthPanel() {
+
+            this.setLayout(new GridLayout());
+
+            //test - 2 bücher erstellt um diese auslesen zu lassen
+            BookModel buch1 = new BookModel("BuchEins");
+            BookModel buch2 = new BookModel("BuchDos");
+            
+            //Erstellen der Tabelle
+            //Spalteninhalt
+            //test - Titel werden ausgelesen
+            String[][] data = {
+                {buch1.getTitel(),"Mustermann", "Max", "123", "true", "1.2", "1990"},
+                {buch2.getTitel(), "Musterfrau", "Maxine", "456", "false", "6.7", "2021"}
+            };
+            // Spaltennamen
+            String[] columnNames = {"Titel", "AutorName", "AutorVorname", "Seitenanzahl", "Gelesen", "Bewertung", "Erscheinungsjahr"};
+            // Initiallisierung
+            JTable tabelle = new JTable(data, columnNames);
+            // in eine ScrollPane einsetzen, macht die tabelle scrollbar
+            JScrollPane sp = new JScrollPane(tabelle);
+            this.add(sp);
+        }
+    
+}
