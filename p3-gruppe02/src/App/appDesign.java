192ca881b99f91d106f4f05bbb5405cb23680c62
JanRipken <72017165+JanRipken@users.noreply.github.com>
2022-11-18 19:12:58 +0100

    background color jtable

--- p3-gruppe02/src/App/appDesign.java
+++ p3-gruppe02/src/App/appDesign.java
@@ -21,7 +21,9 @@ public class appDesign {
         UIManager.put("nimbusFocus", new Color(115, 164, 209));
         UIManager.put("nimbusGreen", new Color(176, 179, 50));
         UIManager.put("nimbusInfoBlue", new Color(66, 139, 221));
-        UIManager.put("nimbusLightBackground", new Color(18, 30, 49));
+        
+        UIManager.put("nimbusLightBackground", Color.LIGHT_GRAY);
+        
         UIManager.put("nimbusOrange", new Color(191, 98, 4));
         UIManager.put("nimbusRed", new Color(169, 46, 34));
         UIManager.put("nimbusSelectedText", new Color(255, 255, 255));
