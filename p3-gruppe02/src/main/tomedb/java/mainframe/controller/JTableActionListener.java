108a3b6491dada4a08ddaacaa4853ec897720398
HungryOtterM2 <114941217+HungryOtterM2@users.noreply.github.com>
2022-11-28 15:09:35 +0100

    Löschen Funktion

--- p3-gruppe02/src/main/tomedb/java/mainframe/controller/JTableActionListener.java
+++ p3-gruppe02/src/main/tomedb/java/mainframe/controller/JTableActionListener.java
@@ -2,20 +2,31 @@ package main.tomedb.java.mainframe.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import main.tomedb.java.mainframe.view.Table;
 import main.tomedb.java.mainframe.view.SouthPanel;
 
 public class JTableActionListener implements ActionListener {
 
+    public JTable table;
+    public DefaultTableModel model;
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
         //Funktioniert nicht, bekomme nicht den richtigen index raus
-        
-        Table.list.deleteBook(e);
-        System.out.println("del");
-        SouthPanel.JlistTabelle.addRowtoTable();
+        removeCurrentRow();
 
     }
 
+    private void removeCurrentRow() {
+        System.out.println("del");
+        this.model = SouthPanel.JlistTabelle.model;
+
+        this.table = SouthPanel.JlistTabelle.table;
+        int selectedRow = table.getSelectedRow();
+        model.removeRow(selectedRow);
+
+    }
 }
