4a266f76b7052a39d9bfeafc5a02b51d9e636512
HungryOtterM2 <114941217+HungryOtterM2@users.noreply.github.com>
2022-12-08 21:13:44 +0100

    clean

--- p3-gruppe02/src/main/tomedb/java/mainframe/view/NorthPanel.java
+++ p3-gruppe02/src/main/tomedb/java/mainframe/view/NorthPanel.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main.tomedb.java.mainframe.view;
 
 import main.tomedb.java.newbookframe.controller.NewBook;
@@ -11,10 +7,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-/**
- *
- * @author janri
- */
 public class NorthPanel extends JPanel {
 
     private JButton newBookButton;
