cb5612c982dc709d17eeaae77f788152c0c61999
JanRipken <72017165+JanRipken@users.noreply.github.com>
2022-12-16 21:43:39 +0100

    testing

--- p3-gruppe02/src/main/tomedb/java/mainframe/controller/JTableSearch.java
+++ p3-gruppe02/src/main/tomedb/java/mainframe/controller/JTableSearch.java
@@ -2,7 +2,6 @@ package main.tomedb.java.mainframe.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.AbstractAction;
 import javax.swing.JTable;
 import javax.swing.RowFilter;
 import javax.swing.table.DefaultTableModel;
@@ -10,7 +9,7 @@ import javax.swing.table.TableRowSorter;
 import main.tomedb.java.mainframe.view.SouthPanel;
 import main.tomedb.java.mainframe.view.TableSearch;
 
-public class JTableSearch extends AbstractAction {
+public class JTableSearch implements ActionListener {
 
     private TableRowSorter<DefaultTableModel> sorter;
     private JTable table;
