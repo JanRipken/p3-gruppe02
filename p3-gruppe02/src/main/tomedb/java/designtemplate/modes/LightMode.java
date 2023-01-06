559a346f1301240a56bca732bd377ac005976082
Jan Ripken <janripken@gmx.com>
2023-01-06 18:42:58 +0100

    lightmode

--- p3-gruppe02/src/main/tomedb/java/designtemplate/modes/LightMode.java
+++ p3-gruppe02/src/main/tomedb/java/designtemplate/modes/LightMode.java
@@ -26,24 +26,22 @@ public class LightMode {
         try {
 
 //            //https://docs.oracle.com/javase/tutorial/uiswing/lookandfeel/_nimbusDefaults.html#primary
+            UIManager.put("control", new Color(255, 255, 255));
+            UIManager.put("info", new Color(255, 255, 255));
+            UIManager.put("nimbusBase", new Color(51, 98, 140));
+            UIManager.put("nimbusAlertYellow", new Color(255, 220, 35));
+            UIManager.put("nimbusDisabledText", new Color(142, 143, 145));
+            UIManager.put("nimbusFocus", new Color(115, 164, 209));
+            UIManager.put("nimbusGreen", new Color(176, 179, 50));
+            UIManager.put("nimbusInfoBlue", new Color(47, 92, 180));
+            UIManager.put("nimbusLightBackground", new Color(255, 255, 255));
+            UIManager.put("nimbusOrange", new Color(191, 98, 4));
+            UIManager.put("nimbusRed", new Color(169, 46, 34));
+            UIManager.put("nimbusSelectedText", new Color(255, 255, 255));
+            UIManager.put("nimbusSelectionBackground", new Color(57, 105, 138));
+            UIManager.put("text", new Color(0, 0, 0));
+            UIManager.put("Menu[MouseOver].backgroundPainter", new FillPainter(Color.LIGHT_GRAY));
 
-
-            UIManager.put("control", new Color(0, 0, 0));
-            UIManager.put("info", new Color(0, 0, 0));
-            UIManager.put("nimbusBase", new Color(0, 0, 0));
-            UIManager.put("nimbusAlertYellow", new Color(0, 0, 0));
-            UIManager.put("nimbusDisabledText", new Color(0, 0, 0));
-            UIManager.put("nimbusFocus", new Color(0, 0, 0));
-            UIManager.put("nimbusGreen", new Color(0, 0, 0));
-            UIManager.put("nimbusInfoBlue", new Color(0, 0, 0));
-            UIManager.put("nimbusLightBackground", Color.red);
-            UIManager.put("nimbusOrange", new Color(0, 0, 0));
-            UIManager.put("nimbusRed", new Color(10, 0, 0));
-            UIManager.put("nimbusSelectedText", new Color(0, 0, 0));
-            UIManager.put("nimbusSelectionBackground", new Color(0, 0, 0));
-            UIManager.put("text", Color.red);
-            UIManager.put("MenuItem[MouseOver].backgroundPainter", new FillPainter(Color.red.brighter()));
-            
         } catch (Exception e) {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         }
